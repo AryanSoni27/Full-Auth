989c6e90131dc79392eb2ab5be315f6de3feae43
AryanSoni27 <aryansoni33635@gmail.com>
2025-03-20 18:45:22 +0530

    Added entities

--- src/main/java/com/auth/full/Entity/UserInfo.java
+++ src/main/java/com/auth/full/Entity/UserInfo.java
@@ -1,4 +1,36 @@
 package com.auth.full.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@TableGenerator(name = "users")
 public class UserInfo {
+
+    @Id
+    @Column(name = "user_id")
+    private String userId;
+
+    private String userName;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+
+    private Set<UserRoles> roles = new HashSet<>();
 }
