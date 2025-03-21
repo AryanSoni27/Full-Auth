5349aeaa6a1bcf769b8633f592e20f4a28afb2a6
AryanSoni27 <aryansoni33635@gmail.com>
2025-03-21 17:38:33 +0530

    Created Refresh token service

--- src/main/java/com/auth/full/Service/RefreshTokenService.java
+++ src/main/java/com/auth/full/Service/RefreshTokenService.java
@@ -1,4 +1,39 @@
 package com.auth.full.Service;
 
+import com.auth.full.Entity.RefreshToken;
+import com.auth.full.Entity.UserInfo;
+import com.auth.full.Repository.RefreshTokenRepo;
+import com.auth.full.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
 public class RefreshTokenService {
+
+    @Autowired
+    private RefreshTokenRepo refreshTokenRepo;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public RefreshToken createRefreshToken(String userName){
+        UserInfo userInfoExtracted = userRepository.findByUserName(userName);
+        RefreshToken refreshToken = RefreshToken.builder()
+                .userInfo(userInfoExtracted)
+                .token(UUID.randomUUID().toString())
+                .expiryDate(Instant.now().plusMillis(600000))
+                .build();
+        return refreshTokenRepo.save(refreshToken);
+    }
+
+    public RefreshToken verifyExpiration(RefreshToken token){
+        if(token.getExpiryDate().compareTo(Instant.now())<0){
+            refreshTokenRepo.delete(token);
+            throw new RuntimeException(token.getToken() + "Error while deleting token");
+        }
+        return token;
+    }
 }
