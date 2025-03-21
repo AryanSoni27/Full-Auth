d7e28ee66a1a056b57119e83f1c0d75407a07332
AryanSoni27 <aryansoni33635@gmail.com>
2025-03-21 10:37:09 +0530

    Create Custom user details service

--- src/main/java/com/auth/full/Service/CustomUserDetails.java
+++ src/main/java/com/auth/full/Service/CustomUserDetails.java
@@ -1,4 +1,66 @@
 package com.auth.full.Service;
 
-public class CustomUserDetails {
+import com.auth.full.Entity.UserInfo;
+import com.auth.full.Entity.UserRoles;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails extends UserInfo implements UserDetails {
+
+    private String userName;
+
+    private String password;
+
+    Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(UserInfo byUserName){
+        this.userName = byUserName.getUserName();
+        this.password = byUserName.getPassword();
+        List<GrantedAuthority> auths = new ArrayList<>();
+
+        for(UserRoles role : byUserName.getRoles()){
+            auths.add(new SimpleGrantedAuthority(role.getName().toUpperCase()));
+        }
+        this.authorities = auths;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
